93b021bf0924449843e5bf4359230177af143aa1
guilhermegouw <guilherme.gouw@gmail.com>
2020-10-11 14:34:23 -0300

    register user completed.

--- account/forms.py
+++ account/forms.py
@@ -7,8 +7,8 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 
-class UserRegistrationForm(forms.Form):
-    password = forms.CharField(label='Password'),
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
 
     class Meta:
@@ -17,6 +17,6 @@ class UserRegistrationForm(forms.Form):
 
     def clean_password2(self):
         cd = self.cleaned_data
-        if cd['password'] == cd['password2']:
+        if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
